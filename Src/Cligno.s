ef191d10a36f4dc33b9a322f791dfab6a6a91d50
Kawtar Essgheir <essgheir@insa-toulouse.fr>
2022-03-27 14:45:39 +0200

    first change

--- Src/Cligno.s
+++ Src/Cligno.s
@@ -1,6 +1,5 @@
 	PRESERVE8
 	THUMB   
-	DriverJeuLaser.inc
 
 ; ====================== zone de réservation de données,  ======================================
 ;Section RAM (read only) :
@@ -10,7 +9,6 @@
 ;Section RAM (read write):
 	area    maram,data,readwrite
 		
-
 	
 ; ===============================================================================================
 	
@@ -19,9 +17,12 @@
 		
 ;section ROM code (read only) :		
 	area    moncode,code,readonly
-; écrire le code ici		
-char FlagCligno;
+	include DriverJeuLaser.inc
 
+; écrire le code ici
+// variable globale
+
+/*char FlagCligno;
 void timer_callback(void)
 {
 	if (FlagCligno==1)
@@ -37,9 +38,31 @@ void timer_callback(void)
 	}
 		
 }
+*/
+;variable globale
+import FlagCligno
 
-
-
+timer_callback proc
+	PUSH {lr,r4,r5}
+	mov r0,#1
+	ldr r4,=FlagCligno
+	ldr r5,[r4]
+	cmp r5,#1
+	bne pegal
+	
+	bl GPIOB_Set
+	mov r5,#0
+	str r5,[r4]
+	b continue
+	pegal
+	
+	bl GPIOB_Set
+	mov r5,#1
+	str r5,[r4]
+	continue
+	POP {lr,r4,r5}
+	BX lr 
+	
 
 		
 		
--- Src/principal.c
+++ Src/principal.c
@@ -1,6 +1,7 @@
 
 
 #include "DriverJeuLaser.h"
+#define periode 7200000 
 
 void timer_callback(void);
 
@@ -15,16 +16,12 @@ int main(void)
 CLOCK_Configure();
 
 // configuration du Timer 4 en débordement 100ms
-	Timer_1234_Init_ff( TIM4, 7200000);
-//** Placez votre code là ** // 
-	
-
+	Timer_1234_Init_ff( TIM4, periode);
 	
 // Activation des interruptions issues du Timer 4
 // Association de la fonction à exécuter lors de l'interruption : timer_callback
 // cette fonction (si écrite en ASM) doit être conforme à l'AAPCS
 	
-//** Placez votre code là ** // 	
 	Active_IT_Debordement_Timer( TIM4, 2, timer_callback );
 
 // configuration de PortB.1 (PB1) en sortie push-pull
@@ -43,5 +40,5 @@ while	(1)
 
 
 		
-}
+
 
