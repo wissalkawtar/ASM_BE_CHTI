e5e747941a2543919af58a5786a33fe8e2261f40
wissalkawtar <kawtaressgheir17@gmail.com>
2022-05-16 10:51:13 +0200

    Début partie DFT Signal reel

---
+++ PjtKEIL_StepDFT/Readme.txt
@@ -0,0 +1,4 @@
+Test partie réelle réussi : le module quand k=1 est égal à 16
+Test partie imaginaire réussi : toutes les val sont nulles
+Test fonction module réussi : la valeur du module quand k=1 est égale à 255
+Test fonction module avec un signal généré par la fonction matlab réussi : val module =256
--- PjtKEIL_StepDFT/Src/principal.c
+++ PjtKEIL_StepDFT/Src/principal.c
@@ -1,7 +1,6 @@
 
 
 #include "DriverJeuLaser.h"
-
 	int tab[64];
 	extern short int LeSignal[] ;
 	extern int DFT_ModuleAuCarre(int ,short int *);	
@@ -17,7 +16,7 @@ int main(void)
 // Après exécution : le coeur CPU est clocké à 72MHz ainsi que tous les timers
 CLOCK_Configure();
 	
-
+ 
 		for (int k=0; k<64; k++){
 				tab[k]=DFT_ModuleAuCarre(k,&LeSignal[0]);
 }
